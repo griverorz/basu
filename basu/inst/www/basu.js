f8ebe286b80789e69ebaa0c6e35551ab3ff0a5d1
Gonzalo Rivero <griverorz@gmail.com>
2018-06-20 15:44:46 -0400

    Bootstrap version

--- basu/inst/www/basu.js
+++ basu/inst/www/basu.js
@@ -1,3 +1,7 @@
+function EH(p1, p2) {
+    return 2 * Math.asin(Math.sqrt(p1)) - 2 * Math.asin(Math.sqrt(p2))
+}
+
 function p_test_param_builder() {
     var params = {};
     params["p1"] = parseFloat($("#p1").val()) || null;
@@ -19,10 +23,10 @@ function p_test_param_builder() {
     return params;
 }
 
+
 function t_test_param_builder() {
     var params = {};
     params["d"] = parseFloat($("#d").val()) || null;
-    params["sd"] = parseFloat($("#sd").val()) || null;
     params["sig.level"] = parseFloat($("#siglevel").val()) || null;
     params["power"] = parseFloat($("#power").val()) || null;
     params["alternative"] = $("#alternative").val();
@@ -39,57 +43,18 @@ function t_test_param_builder() {
 }
 
 
-function EH(p1, p2) {
-    return 2 * Math.asin(Math.sqrt(p1)) - 2 * Math.asin(Math.sqrt(p2))
-}
-
-
 $(document).ready(function() {
-    $("#p1p").click(function() {
+    $("#ptest").click(function() {
         var params = p_test_param_builder();
-        var req = ocpu.rpc("pwr.p.test",
+        var f = String($("input[name=pname]:checked").val());
+        var req = ocpu.rpc(f,
                            params,
                            function(output) {
-                               $("#output").text(JSON.stringify(params));
-                           });
-        
-        req.fail(function() {
-            alert("Error: " + req.responseText);
-        });
-    });
-    
-    $("#p2p").click(function() {
-        var params = p_test_param_builder();
-        var req = ocpu.rpc("pwr.2p.test",
-                           params,
-                           function(output) {
-                               $("#output").text(JSON.stringify(params));
-                           });
-        
-        req.fail(function() {
-            alert("Error: " + req.responseText);
-        });
-    });
-    
-    $("#t1p").click(function() {
-        var params = t_test_param_builder();
-        var req = ocpu.rpc("pwr.t.test",
-                           params,
-                           function(output) {
-                               $("#output").text(JSON.stringify(params));
-                           });
-        
-        req.fail(function() {
-            alert("Error: " + req.responseText);
-        });
-    });
-    
-    $("#t2p").click(function() {
-        var params = t_test_param_builder();
-        var req = ocpu.rpc("pwr.2tn.test",
-                           params,
-                           function(output) {
-                               $("#output").text(JSON.stringify(params));
+                               for (var key in output) {
+                                   n = String("#tab".concat(key));
+                                   n = n.split('.').join("");
+                                   $(n).text(output[key]);
+                               }
                            });
         
         req.fail(function() {
