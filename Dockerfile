c59aecdc943b9b7b41c2644c17d075c0724402a5
Gonzalo Rivero <griverorz@gmail.com>
2018-06-21 13:50:52 -0400

    Better README

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM opencpu/base
+
+# install latex packages
+RUN apt-get update -y \
+    && apt-get install -y \
+    pandoc
+
+COPY /basu /basu
+WORKDIR /basu
+
+EXPOSE 5656
+
+ENTRYPOINT ["Rscript"]
+CMD ["setup.R"]
+
+
---
+++ setup.R
@@ -0,0 +1,19 @@
+r <- getOption('repos')
+r['CRAN'] <- 'http://cran.us.r-project.org'
+options(repos = r)
+
+install.packages(c("rmarkdown",
+                   "htmltools",
+                   "flexdashboard"))
+
+rmarkdown::render(input="index.Rmd", output_file="basu/inst/www/index.html")
+
+if ("basu" %in% rownames(installed.packages())) {
+    devtools::uninstall("basu")
+}
+
+devtools::build("./basu")
+devtools::install("./basu")
+
+opencpu:::ocpu_start_server()
+browseURL("http://localhost:5656/ocpu/library/basu/www/index.html")
