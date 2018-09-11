01d9ebc5fbd95dec7b089053da436b1d6ee970b1
Gonzalo Rivero <gonzalorivero@air.local>
2018-09-11 12:42:15 -0400

    Test for missing sig.level cannot succeed

--- basu/tests/testthat/test-missing.R
+++ basu/tests/testthat/test-missing.R
@@ -1,13 +1,13 @@
 context("Captures missing variable")
 
 test_that("Captures missing h in v.pwr.p.test",
-    expect_that(v.pwr.p.test(n=100, sig.level=0.05, power=0.8, deff=1)$calculated_covar, "h"))
+    expect_equal(v.pwr.p.test(n=100, sig.level=0.05, power=0.8, deff=1)$calculated_covar, "h"))
 
 test_that("Captures missing n in v.pwr.p.test",
-    expect_that(v.pwr.p.test(h=1, sig.level=0.05, power=0.8, deff=1)$calculated_covar, "n"))
-
-test_that("Captures missing sig.level in v.pwr.p.test",
-    expect_that(v.pwr.p.test(h=1, n=50, power=0.8, deff=1)$calculated_covar, "sig.level"))
+    expect_equal(v.pwr.p.test(h=1, sig.level=0.05, power=0.8, deff=1)$calculated_covar, "n"))
 
 test_that("Captures missing power in v.pwr.p.test",
-    expect_that(v.pwr.p.test(h=1, n=50, sig.level=0.05, deff=1)$calculated_covar, "power"))
+    expect_equal(v.pwr.p.test(h=1, n=50, sig.level=0.05, deff=1)$calculated_covar, "power"))
+
+test_that("Errors if missing sig.level in v.pwr.p.test",
+    expect_error(v.pwr.p.test(h=1, n=50, power=0.8, deff=1)$calculated_covar))
