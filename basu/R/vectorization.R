2b09bac9efa52d3496aa37572e8fb63d1f4cee9c
Gonzalo Rivero <gonzalorivero@air.local>
2018-09-13 11:35:42 -0400

    Calculate mdd from R pkg

--- basu/R/vectorization.R
+++ basu/R/vectorization.R
@@ -1,15 +1,21 @@
 #' Vectorized pwr.2p.test
 #' @export
-v.pwr.2p.test <- function(..., deff=1) {
+v.pwr.2p.test <- function(..., deff=1, p2=NULL, h=NULL) {
     out <- vector_pwr(..., pwrf=pwr:::pwr.2p.test, deff=deff)
+    if (is.null(h) & !is.null(p2)) {
+        out$mdd <- p1_from_ES(out$h, p2)
+    }    
     return(out)
 }
 
 
 #' Vectorized pwr.p.test
 #' @export
-v.pwr.p.test <- function(..., deff=1) {
+v.pwr.p.test <- function(..., deff=1, p2=NULL, h=NULL) {
     out <- vector_pwr(..., pwrf=pwr:::pwr.p.test, deff=deff)
+    if (is.null(h) & !is.null(p2)) {
+        out$mdd <- p1_from_ES(out$h, p2)
+    }
     return(out)
 }
 
@@ -35,4 +41,10 @@ v.pwr.r.test <- function(..., deff=1) {
 cohen.ES <- function(...) {
     out <- pwr:::cohen.ES(...)
     return(unclass(out))
+}
+
+##' Calculate difference from effect size
+p1_from_ES <- function(h, p2, alternative) {
+    p1 <- sin((h + 2*asin(sqrt(p2)))/2)^2
+    return(p1)
 }
